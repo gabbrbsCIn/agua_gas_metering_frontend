a887ea5c88b0011b1f9e7c948bde016a895e75be
Gabriel Barbosa <gbgo@cin.ufpe.br>
2024-09-16 11:39:09 -0300

    feat: add componente CreateMeasureButton

---
+++ frontend/src/components/createMeasureButton.js
@@ -0,0 +1,34 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+const CreateMeasureButton = ({ selectedType }) => {
+  const [customerCode, setCustomerCode] = useState("");
+  const navigate = useNavigate();
+
+  const handleSubmit = () => {
+    if (customerCode) {
+      navigate(`/${selectedType}/${customerCode}`);
+    }
+  };
+
+  return (
+    <div className="flex flex-col space-y-4 items-center">
+      <input
+        className="rounded-lg w-full px-3 py-2 border border-gray-300 hover:border-gray-400 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50"
+        type="text"
+        value={customerCode}
+        placeholder="Digite o Customer Code"
+        onChange={(e) => setCustomerCode(e.target.value)}
+      />
+      <button
+        className="rounded-lg bg-blue-500 hover:bg-blue-700 text-white px-4 py-2 font-medium"
+        onClick={handleSubmit}
+        disabled={!customerCode}
+      >
+        Continuar para a captura de imagem
+      </button>
+    </div>
+  );
+};
+
+export default CreateMeasureButton;
