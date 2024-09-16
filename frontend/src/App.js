cd23dcc830191951d8327a6d5f34fc7193a1a197
Gabriel Barbosa <gbgo@cin.ufpe.br>
2024-09-16 10:12:20 -0300

    feat: add rotas do frontend ao App

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,15 +2,16 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/home";
 import ListMeasures from "./pages/listMeasures";
 
-
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home/>} />
-        <Route path="/measures" element={<ListMeasures/>} />
-      </Routes>
-    </BrowserRouter>
+    <div className="bg-blue-900 w-screen h-screen">
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Home />} />
+          <Route path="/measures" element={<ListMeasures />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
   );
 }
 
